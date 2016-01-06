e1f7bec6063676a7bc4525720c47a668cfe05218
yletaillanter <y.letaillanter@gmail.com>
2016-01-06 17:05:36 +0100

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name geocovApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the geocovApp
+ */
+angular.module('geocovApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name geocovApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the geocovApp
+ */
+angular.module('geocovApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
